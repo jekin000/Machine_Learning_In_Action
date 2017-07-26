8d5b93465cc08c6dd49edf1ad152afa05d2ae6f0
jekin000 <kira_r163@163.com>
2017-07-26 01:50:09 +0000

    [13]-ch04 calc p0Vect/p1Vect/pAbusive

--- ch04/bayes.py
+++ ch04/bayes.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def loadDataSet():
 	postingList = [
 		['my','dog','has','flea','problems','help','please']
@@ -5,6 +7,7 @@ def loadDataSet():
 		,['my','dalmation','is','so','cute','I','love','him']
 		,['stop','posting','stupid','worthless','garbage']
 		,['mr','licks','ate','my','steak','how','to','stop','him']
+		,['quit','buying','worthless','dog','food','stupid']
 	]
 
 	classVec = [0,1,0,1,0,1]
@@ -26,4 +29,27 @@ def setOfWords2Vec(vocabList,inputSet):
 	return returnVec
 
 
+def trainNB0(trainMatrix,trainCategory):
+	#calc doc's p1, p0 = 1 - p1
+	numTrainDocs = len(trainMatrix)
+	pAbusive = sum(trainCategory) / float(numTrainDocs)
+
+	#calc p(w|ci) , p(w|ci) =  sum_word_matrix(docs)/sum_word_count(ci)
+	numWords = len(trainMatrix[0])
+	p0Num = np.zeros(numWords)
+	p1Num = np.zeros(numWords)
+	p0Denom = 0.0
+	p1Denom = 0.0
+	for i in range(numTrainDocs):
+		if trainCategory[i] == 1:
+			p1Num += trainMatrix[i]
+			p1Denom += sum(trainMatrix[i])
+		else:
+			p0Num += trainMatrix[i]
+			p0Denom += sum(trainMatrix[i])
+
+	p0Vect = p0Num/p0Denom
+	p1Vect = p1Num/p1Denom 
+
+	return p0Vect,p1Vect,pAbusive
 
--- commit.sh
+++ commit.sh
@@ -1,1 +1,1 @@
-git commit -m "[12]-ch04 test in office"
+git commit -m "[13]-ch04 calc p0Vect/p1Vect/pAbusive"
